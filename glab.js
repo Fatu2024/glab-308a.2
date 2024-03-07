8735c48d4cea227d9dfdce7651ef9037e2d4c96b
Fatu2024 <fatumab3@gmail.com>
2024-03-07 14:40:34 -0500

    part 2 done

--- glab.js
+++ glab.js
@@ -2,7 +2,7 @@
 // We will call the adventurer “Robin.”
 const adventurer = {
     name: "Robin",
-    health: 10,
+    health: 100,
     inventory: ["sword", "potion", "artifact"],
 
     // Next, give Robin’s feline friend a friend of his own:
@@ -15,10 +15,53 @@ const adventurer = {
             inventory: "small hat and sunglasses",
         }
     },
-//give Robin the following method
+    //give Robin the following method
     roll(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`);
     }
 };
 adventurer.roll();
+console.log('------');
+
+//Part 2: Class Fantasy
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+console.log(robin);
+console.log(robin.companion.type);
+// console.log(Character);
+robin.companion.companion.roll();
+
+//Part 3: Class Features
+class Adventurer extends Character {
+    constructor(name, role) {
+        super(name);
+        // Adventurers have specialized roles.
+        this.role = role;
+        // Every adventurer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+}
